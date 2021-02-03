59cda0051cdfc66232b597a8a05f2ec3af8288a3
Birzhan Serik <180107167@stu.sdu.edu.kz>
2021-02-03 20:43:17 +0600

    add form component & styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from './components/header/header'
 import ShopPage from './pages/shop/shop'
 import HomePage from './pages/homepage/homepage'
+import Auth from './pages/auth/auth'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/auth' component={Auth} />
         <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
---
+++ src/pages/auth/auth.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './auth.scss';
+
+import SignIn from '../../components/sign-in/sign-in'
+
+
+const Auth = () => (
+	<div className='auth'>
+		<SignIn />
+	</div>
+);
+
+export default Auth;
