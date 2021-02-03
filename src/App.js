c7366820feb0485cc6248c2e191e8f6332bddbda
Birzhan Serik <180107167@stu.sdu.edu.kz>
2021-02-03 18:13:02 +0600

    add header

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/header/header'
 import ShopPage from './pages/shop/shop'
 import HomePage from './pages/homepage/homepage'
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-    </Switch>
-
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
